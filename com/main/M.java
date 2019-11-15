e06475a8355bcb0b446fc92ba22152e27cc9bf94
fenfeizhiyu <fenfeizhiyu@sian.com>
2019-11-15 18:58:14 +0800

    aop spring

--- com/main/M.java
+++ com/main/M.java
@@ -1,7 +1,31 @@
 package main;
 
+import model.CalcNum;
+
+
+
 public class M {
     public static void main(String[] args){
-        System.out.println(13.310546875%1==0);
+        InheritableThreadLocal<CalcNum> local_1 = new InheritableThreadLocal<CalcNum>();
+        CalcNum num = new CalcNum(50);
+        local_1.set(num);
+        Runnable task=new Runnable() {
+            @Override
+            public void run() {
+               CalcNum num=local_1.get();
+               num.setNum(num.getNum()+1);
+                System.out.println(Thread.currentThread().getName()+":"+local_1.get().getNum());
+            }
+
+        };
+        Thread[] threads=new Thread[5];
+        for(int i=0;i<threads.length;i++){
+            threads[i] = new Thread(task);
+            threads[i].setName("threadName_" + i);
+        }
+        for (Thread t : threads) {
+            t.start();
+        }
+
     }
 }
---
+++ com/model/CalcNum.java
@@ -0,0 +1,19 @@
+package model;
+
+public class CalcNum {
+
+
+    private Integer num=10;
+
+    public CalcNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
